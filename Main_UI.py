c525f30cd2d8b98770eed0a9e8ddc4252d424671
Trần Hùng Cường <93498241+trancuong31@users.noreply.github.com>
2024-10-28 10:27:08 +0700

    Update Main_UI.py

--- Main_UI.py
+++ Main_UI.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from GUI import Ui_MainWindow
-from PyQt5.QtCore import QThread, Qt
+from PyQt5.QtCore import QThread
 from PyQt5 import QtWidgets
 from mainLogic import MainLogic
 
@@ -23,16 +23,17 @@ class MainWindow(QMainWindow):
         # Khởi tạo MainLogic
         self.mainLogic = MainLogic()
         self.mainLogic.update_signal.connect(self.updateUI)
-        
-        # Sử dụng QThread cho logic
         self.logic_thread = WorkerThread(self.mainLogic)
         self.logic_thread.start()
 
     def updateUI(self, listMachineStatus):
-        self.ui.dgvMachineStatus.setRowCount(len(listMachineStatus))
-        
+        current_row_count = self.ui.dgvMachineStatus.rowCount()
+        new_row_count = len(listMachineStatus)
+        if new_row_count > current_row_count:
+            self.ui.dgvMachineStatus.setRowCount(new_row_count)
+        scroll_position = self.ui.dgvMachineStatus.verticalScrollBar().value()
         for row, machine in enumerate(listMachineStatus):
-            self.ui.dgvMachineStatus.setItem(row, 0, QtWidgets.QTableWidgetItem(machine.clNameMachine)) 
+            self.ui.dgvMachineStatus.setItem(row, 0, QtWidgets.QTableWidgetItem(machine.clNameMachine))
             self.ui.dgvMachineStatus.setItem(row, 1, QtWidgets.QTableWidgetItem(machine.clStatus))
             self.ui.dgvMachineStatus.setItem(row, 2, QtWidgets.QTableWidgetItem(machine.clIpaddr))
             self.ui.dgvMachineStatus.setItem(row, 3, QtWidgets.QTableWidgetItem(machine.Cltime))
@@ -41,19 +42,20 @@ class MainWindow(QMainWindow):
             self.ui.dgvMachineStatus.setItem(row, 6, QtWidgets.QTableWidgetItem(machine.clRed))
             self.ui.dgvMachineStatus.setItem(row, 7, QtWidgets.QTableWidgetItem(machine.clIDLE))
             self.ui.dgvMachineStatus.setItem(row, 8, QtWidgets.QTableWidgetItem(machine.clError))
-            self.ui.dgvMachineStatus.setItem(row, 9, QtWidgets.QTableWidgetItem(machine.clConnect))
+            self.ui.dgvMachineStatus.setItem(row, 9, QtWidgets.QTableWidgetItem(str(machine.clConnect)))
 
-        self.ui.dgvMachineStatus.setColumnWidth(0, 100)
-        self.ui.dgvMachineStatus.setColumnWidth(1, 100) 
+        self.ui.dgvMachineStatus.setColumnWidth(0, 100)                    
+        self.ui.dgvMachineStatus.setColumnWidth(1, 100)
         self.ui.dgvMachineStatus.setColumnWidth(2, 100) 
         self.ui.dgvMachineStatus.setColumnWidth(3, 150) 
         self.ui.dgvMachineStatus.setColumnWidth(4, 100) 
-        self.ui.dgvMachineStatus.setColumnWidth(5, 100) 
+        self.ui.dgvMachineStatus.setColumnWidth(5, 100)
         self.ui.dgvMachineStatus.setColumnWidth(6, 100)
-        self.ui.dgvMachineStatus.setColumnWidth(7, 100) 
-        self.ui.dgvMachineStatus.setColumnWidth(8, 235) 
+        self.ui.dgvMachineStatus.setColumnWidth(7, 100)
+        self.ui.dgvMachineStatus.setColumnWidth(8, 235)
         self.ui.dgvMachineStatus.setColumnWidth(9, 100)
 
+        self.ui.dgvMachineStatus.verticalScrollBar().setValue(scroll_position)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
